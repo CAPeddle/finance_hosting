6d21fb0687d2b8ee672873fa6122c695bf83d7a4
cpeddle <chrisapeddle@gmail.com>
2024-10-25 21:51:45 +0200

    include translation of column names

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 import pandas as pd
 
+def translate_columns(df, translation_dict):
+    return df.rename(columns=translation_dict)
+
 def combine_xls_files(file1, file2, output_file):
+    # Translation dictionary from Dutch to English
+    translation_dict = {
+        'Rekeningnummer': 'accountNumber',
+        'Muntsoort': 'mutationcode',
+        'Transactiedatum': 'transactiondate',
+        'Rentedatum': 'valuedate',
+        'Beginsaldo': 'startsaldo',
+        'Eindsaldo': 'endsaldo',
+        'Transactiebedrag': 'amount',
+        'Omschrijving': 'description'
+    }
+    
     # Read the two XLS files
     df1 = pd.read_excel(file1)
     df2 = pd.read_excel(file2)
@@ -8,6 +23,10 @@ def combine_xls_files(file1, file2, output_file):
     # Remove leading empty columns if any
     df1 = df1.loc[:, ~df1.columns.str.match('Unnamed')]
     df2 = df2.loc[:, ~df2.columns.str.match('Unnamed')]
+    
+    # Translate Dutch columns to English
+    df1 = translate_columns(df1, translation_dict)
+    df2 = translate_columns(df2, translation_dict)
     
     # Combine the dataframes
     combined_df = pd.concat([df1, df2], ignore_index=True)
